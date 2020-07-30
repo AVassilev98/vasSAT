b5057e6db69acdfea1f748abf82b5d7ec1c8398b
Anton <avvassil@uwaterloo.ca>
2020-07-30 18:27:08 -0400

    tweak reading for CNF

--- lib/vasSAT/FileUtils.cpp
+++ lib/vasSAT/FileUtils.cpp
@@ -13,22 +13,22 @@ std::shared_ptr<vasSAT::CNFFormula> parseCNFFile(const std::string &path) {
   auto formula = make_shared<vasSAT::CNFFormula>();
 
   ifstream ifs;
-  std::string line;
+  std::string cnfLine;
   ifs.open(path);
   if (ifs.is_open()) {
     while (!ifs.eof()) {
-      std::getline(ifs, line);
+      getline(ifs, cnfLine);
       // skip initial comments and p line
-      if (line[0] == 'c' || line[0] == 'p') continue;
+      if (cnfLine[0] == 'c' || cnfLine[0] == 'p') continue;
 
       std::vector<int> clause;
-      for (unsigned i = 0; i < line.size(); ++i) {
-        if (line[i] == ' ') continue;
-        if (line[i] == '0')
+      for (unsigned i = 0; i < cnfLine.size(); ++i) {
+        if (cnfLine[i] == ' ') continue;
+        if (cnfLine[i] == '0' && i == cnfLine.size() - 1) continue;
+        if (cnfLine[i] == '0' && i != cnfLine.size() - 1)
           std::cout << "Variables must start at one!: " << path << std::endl;
-        if (i == 0 || line[i - 1] == ' ') {
-          string::size_type sz = i;
-          int var = stoi(line, &sz);
+        if (i == 0 || cnfLine[i - 1] == ' ') {
+          int var = atoi(&cnfLine.c_str()[i]);
 
           if (var == 0)
             std::cout << "Unkown symbol found!: " << path << std::endl;
