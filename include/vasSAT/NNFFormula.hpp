82f6070567db4f5424650c4bb4a82c186f8a44a0
Anton <avvassil@uwaterloo.ca>
2020-08-01 00:25:06 -0400

    Refactor NNFFormula
    no unnecessary setters

--- include/vasSAT/NNFFormula.hpp
+++ include/vasSAT/NNFFormula.hpp
@@ -13,24 +13,19 @@ enum class NodeType;
 
 class NNFFormula {
 private:
+  friend class Parser;
+  NodeRef m_rootNode = nullptr;
+
   NNFFormula() = default;
 
   // each node will be inserted by its internal ID
-  NodeRef m_rootNode;
 
   void checkNoCycles() const;
   void checkTerminates() const;
 
   void inorder(const NodeRef &node, std::string &str) const;
 
-  // returns the ID of the node you add
-  unsigned addNode(std::optional<unsigned> externalID, NodeType type);
-  void setLeft(unsigned curID, unsigned leftId);
-  void setRight(unsigned curID, unsigned rightID);
-
 public:
-  friend class Parser;
-
   void printExternalToInternal(std::ostream &os) const;
   void print(std::ostream &os) const;
 };
